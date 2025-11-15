92641ccf1c01ffce5ae8f6543ac025023bb61ac3
Jorge Alejandro Vesga Quintero <102545608+Jvesga@users.noreply.github.com>
2025-11-15 11:02:34 -0500

    Add files via upload

---
+++ Ejercicio-1.py
@@ -0,0 +1,38 @@
+#Leer una matriz de orden M*A y un numero K
+#Multiplicar todos los elementos de la matriz por el numero K
+#Mostrar la matriz resultante
+
+matriz=[]
+resultante=[]
+
+K= int(input("Digite un numero entero: "))
+M= int(input("Digite numero de filas: "))
+A= int(input("Digite numero de columnas: "))
+
+for i in range (M):
+    matriz.append([])
+    for j in range(A):
+        matriz[i].append(None)
+
+for j in range (0,M):
+    for i in range(0,A):
+        matriz[j][i]=int(input("Digite un numero: "))
+
+
+
+for a in range (M):
+    resultante.append([])
+    for b in range(A):
+        resultante[a].append(None)
+
+for c in range(0,M):
+    for d in range(0,A):
+        resultante[c][d]= matriz[c][d] * K
+
+
+print("\nMatriz resultante:")
+for M in resultante:
+    for elemento in M:
+        print(f"{elemento}", end=" ")
+    print()
+
